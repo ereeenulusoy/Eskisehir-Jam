ea0f0fd21c99478890cde9130b989f69f15078c9
youjirohanma <erenulusoy961@gmail.com>
2026-05-07 12:24:49 +0300

    Ragdoll

---
+++ Assets/Scripts/RagdollController.cs
@@ -0,0 +1,81 @@
+using UnityEngine;
+using StarterAssets;
+
+public class RagdollController : MonoBehaviour
+{
+    private Animator _animator;
+    private CharacterController _characterController;
+    private ThirdPersonController _tpController;
+    private StarterAssetsInputs _input;
+
+    // Mahluk'un içindeki tüm kemik fizikleri
+    private Rigidbody[] _boneRigidbodies;
+
+    void Start()
+    {
+        _animator = GetComponent<Animator>();
+        _characterController = GetComponent<CharacterController>();
+        _tpController = GetComponent<ThirdPersonController>();
+        _input = GetComponent<StarterAssetsInputs>();
+
+        // Karakterin altýndaki (kemiklerdeki) tüm Rigidbody'leri bul
+        _boneRigidbodies = GetComponentsInChildren<Rigidbody>();
+
+        // Oyun baþlarken Ragdoll'u kapalý tut
+        SetRagdollState(false);
+    }
+
+    // Karakter KATIK BÝR CÝSME fiziksel olarak çarptýðýnda Unity bu metodu çaðýrýr
+    private void OnControllerColliderHit(ControllerColliderHit hit)
+    {
+        // Çarptýðýmýz þeyin etiketi Obstacle ise
+        if (hit.gameObject.CompareTag("Obstacle"))
+        {
+            // hit.normal = Çarptýðýmýz yüzeyin bize doðru olan dik açýsý (geri tepme yönü)
+            Die(hit.normal);
+        }
+    }
+
+    // Die metoduna artýk engelin bizi ne tarafa ittiði bilgisini (impactNormal) veriyoruz
+    public void Die(Vector3 impactNormal)
+    {
+        SetRagdollState(true);
+
+        // Karakterin hareket okumasýný durdur
+        if (_input != null)
+        {
+            _input.isMovementStarted = false;
+        }
+
+        // Doðal fizik hesaplamasý
+        foreach (Rigidbody rb in _boneRigidbodies)
+        {
+            // 1. Karakterin koþudan gelen kendi ileri ivmesi (biraz azaltýlmýþ hali)
+            Vector3 runMomentum = transform.forward * 4f;
+
+            // 2. Senin bahsettiðin o doðal "pulse" - engelden karaktere doðru seken güç
+            Vector3 bouncePulse = impactNormal * 6f;
+
+            // Ýkisini birleþtirip kemiklere anlýk güç (Impulse) olarak uyguluyoruz
+            rb.AddForce(runMomentum + bouncePulse, ForceMode.Impulse);
+        }
+    }
+
+    private void SetRagdollState(bool isRagdollActive)
+    {
+        // 1. Eðer Ragdoll aktifse, Standart Kontrolleri ve Animasyonu KAPAT
+        if (_animator != null) _animator.enabled = !isRagdollActive;
+        if (_characterController != null) _characterController.enabled = !isRagdollActive;
+        if (_tpController != null) _tpController.enabled = !isRagdollActive;
+
+        // 2. Kemik fiziklerini yönet
+        foreach (Rigidbody rb in _boneRigidbodies)
+        {
+            // Kendi ana objemizde bir Rigidbody varsa onu atla
+            if (rb.gameObject == gameObject) continue;
+
+            // Ragdoll aktifse Kinematic kapanýr (fizik motoru devralýr)
+            rb.isKinematic = !isRagdollActive;
+        }
+    }
+}

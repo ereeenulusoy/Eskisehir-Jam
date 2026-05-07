1b884bf507cae813c4ed4989de7211086e3a140e
youjirohanma <erenulusoy961@gmail.com>
2026-05-07 09:06:30 +0300

    New characters & animations & camera angles

--- Assets/Scripts/LevelTransitionTrigger.cs
+++ Assets/Scripts/LevelTransitionTrigger.cs
@@ -1,6 +1,6 @@
 using UnityEngine;
 using StarterAssets;
-using Cinemachine; // Bunu eklemeyi unutma!
+using Cinemachine;
 
 public class LevelTransitionTrigger : MonoBehaviour
 {
@@ -9,10 +9,14 @@ public class LevelTransitionTrigger : MonoBehaviour
     public bool newCanJump;
     public bool newCanControlCharacterHorizontal;
     public bool newIsCameraInverted;
+    public bool newCanUseShiftToSlowDown;
 
     [Header("Kamera Kontrolü")]
     [Tooltip("Bu bölüme geçince aktif olacak kamera")]
     public CinemachineVirtualCamera targetCamera;
+
+    [Tooltip("Bu kameranýn öncelik gücü. Bir önceki leveldan DAHA YÜKSEK olmalý.")]
+    public int cameraPriority = 20; // <--- YENÝ EKLENDÝ
 
     private void OnTriggerEnter(Collider other)
     {
@@ -26,12 +30,13 @@ public class LevelTransitionTrigger : MonoBehaviour
                 input.canJump = newCanJump;
                 input.canControlCharacterHorizontal = newCanControlCharacterHorizontal;
                 input.isCameraInverted = newIsCameraInverted;
+                input.canUseShiftToSlowDown = newCanUseShiftToSlowDown;
 
                 // Kamerayý Deðiþtir
                 if (targetCamera != null)
                 {
-                    // Diðer tüm kameralarýn önceliðini aþmak için yüksek bir deðer ver
-                    targetCamera.Priority = 20;
+                    // Artýk önceliði Inspector'dan belirlediðimiz sayýya çekiyoruz
+                    targetCamera.Priority = cameraPriority;
                 }
 
                 gameObject.SetActive(false);
